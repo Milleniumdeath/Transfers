45d900194de909e8a3e203c9729318a446b135b7
Millenium death <milleniumdeath90@gmail.com>
2025-06-05 10:39:46 +0500

    bazi joylarga o‘zgartirishlar qilindi

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,14 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from main.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('clubs/', ClubsView.as_view(), name='clubs'),
+    path('clubs/<int:pk>/',ClubInfoView.as_view(), name='club-info' ),
+    path('latest-transfers', LatestTransfersView.as_view(), name='latest-transfers' )
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+
+from django.views import View
+from .models import *
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class ClubsView(View):
+    def get(self, request):
+        clubs = Club.objects.all()
+        context = {
+            'clubs': clubs
+        }
+        return render(request, 'clubs.html', context)
+
+
+class ClubInfoView(View):
+    def get(self, request, pk):
+        club = get_object_or_404(Club, id=pk)
+        players = club.player_set.order_by('-price')
+        context = {
+            'club': club,
+            'players': players
+        }
+        return render(request, 'clubs-info.html', context)
+
+class LatestTransfersView(View):
+    def get(self, request):
+        transfers = Transfer.objects.filter(season=Season.objects.last()).order_by('-price')
+        context = {
+            'transfers': transfers,
+        }
+        return render(request, 'latest-transfers.html', context)
